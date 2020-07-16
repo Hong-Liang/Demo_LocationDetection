01c3cea584eebaeff075a89091f715401ab6315b
Hong-Liang <57203229+Hong-Liang@users.noreply.github.com>
2020-07-16 10:29:41 +0800

    Final commit

---
+++ app/src/main/java/com/myapplicationdev/android/demolocationdetection/MainActivity.java
@@ -0,0 +1,102 @@
+package com.myapplicationdev.android.demolocationdetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGetLastLocation, btnReceiveUpdate, btnRemoveUpdate;
+    FusedLocationProviderClient client;
+    LocationRequest mLocationRequest;
+    LocationCallback mLocationCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetLastLocation = findViewById(R.id.btnGetLastLocation);
+        btnReceiveUpdate = findViewById(R.id.btnGetLocationUpdate);
+        btnRemoveUpdate = findViewById(R.id.btnRemoveLocationUpdate);
+        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        btnGetLastLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Task<Location> task = client.getLastLocation();
+                if (checkPermission() == true) {
+                    task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            //Get last known location.
+                            if (location != null) {
+                                String msg = "Lat : " + location.getLatitude() +
+                                        "\nLng : " + location.getLongitude();
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            } else {
+                                String msg = "No last known location found";
+                                Toast.makeText(MainActivity.this,msg , Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        final LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                if (locationResult != null){
+                    Location locData = locationResult.getLastLocation();
+                    String msg = "New Loc Detected\n" +
+                            "Lat: " + locData.getLatitude() + "\n" +
+                            "Lng: " + locData.getLongitude();
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnReceiveUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermission() == true){
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                }
+            }
+        });
+    }
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
